31267d65a25231bb1eb73769ab141ed2be6c7101
vince095 <vincent_thejane@outlook.com>
2021-02-22 15:13:26 +0800

    bug fix on CORS

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ function news(country,cat,publisher){
     if(cat!=null){
         cat='&category='+cat
     }else{cat=""}
-  var url='https://newsapi.org/v2/top-headlines?country='+country+cat+'&apiKey=8f1d454aaba44826859155d0c350eda2';
+  var url='http://newsapi.org/v2/top-headlines?country='+country+cat+'&apiKey=f1f74c922b344c268af95738220fbe63';
 $.ajax({
         url:url,
-        beforeSend: function(xhr) {
-             xhr.setRequestHeader("Authorization", "Bearer 6QXNMEMFHNY4FJ5ELNFMP5KRW52WFXN5")
-        }, success: function(data){
-        $("#list").html("failed to fetch data");
-            
-            for(var i=0;i<data["articles"].length;i++){
-           $("#list").append("<img class='thmb' src="+data['articles'][i]['urlToImage']+"><br><b><a href="+data['articles'][i]['url']+">"+data['articles'][i]['title']+"</a></b><br><small>by"+data['articles'][i]['author']+"<br>"+data['articles'][i]['publishedAt']+"<br></small><div class='less' onclick=h(this)><div class='content'>"+data['articles'][i]['content']+"<br></div><small>Read more..</small></div><hr>")
-        }}
+        "timeout":0
         
-})}
+}).done( function(data){
+    $("#list").html(`<h2><u style='color:#7CB342'>Top headlines - ${country}</u></h2>`);
+        
+        for(var i=0;i<data["articles"].length;i++){
+           // console.log(data["articles"][i]['title'])
+       $("#list").append("<img class='thmb' src="+data['articles'][i]['urlToImage']+"><br><b><a href="+data['articles'][i]['url']+">"+data['articles'][i]['title']+"</a></b><br><small>by "+data['articles'][i]['author']+"<br>"+data['articles'][i]['publishedAt']+"<br></small><div class='less' onclick=h(this)><div class='content'>"+data['articles'][i]['content']+"<br></div><small>Read more..</small></div><hr>")
+    }}).catch(e =>{console.log(e.message)})}
+
+
     $("#country").on("change",function(){
        news($("#country").val(),$("#cat").val(),"") 
     })
